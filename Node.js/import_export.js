69b41828c278c16c53d53e4f22395a65f2a1844a
ShevchenkoOl <uzlabini@gmail.com>
2025-07-22 11:24:07 +0200

    added interview and express.js

--- Node.js/import_export.js
+++ Node.js/import_export.js
@@ -21,7 +21,7 @@ export default { //Экспорт по умолчанию (если хочешь
 
 // index.js
 import { users, getAllContacts} from './users.js';
-
+import books from "./books.json" assert { type: "json" }; // але звлежить від версії node новіші версії не дуже коректно з ним працюють
 // якщо ми імпортували я к один об'єкт (export default)
 import contacts from './contacts.js';
 
@@ -29,19 +29,30 @@ contacts.getAllContacts();
 contacts.addContact();
 
 
-
-
 // Для вбудованих модулів:
 import fs from 'fs';
 import path from 'path'; 
 
+// Коли ми працбємо з імпортом json файлів, може бути різного роду помилки через версії node.js, тоді краще зчитувати інформацію з нього через:
+const fs = require("fs/promises");
+
+app.get("/books", async (req, res) => {
+  const data = await readFile("./books.json", "utf-8");
+  const books = JSON.parse(data);
+  const html = books.map(book => `
+      <h1>${book.title}</h1>
+      <span>${book.description}</span>
+    `).join("");
+  res.send(html);
+});
 
 
 
 
 
+//--------------------CommonJS більш старіший спосіб імрорту/експорту
+// "type": "module" удаляємо з package.json!!!!!!!!!!!!!!!
 
-//--------------------CommonJS більш старіший спосіб імрорту/експорту  
 // Цей вид експорту-імпорту називаеться CommonJS 
 // При експорті:
 const admins = ["Alex", "Tanja", "Denis"];
@@ -59,6 +70,7 @@ module.exports = {
 
 
 // При імпорті:
+const books = require("./books.json"); // звичайний json
 const nodemon = require('nodemon'); // якщо ми імпортуємо з пакетів, просто вказуєм назву пакетв
 const obj = require("./index2"); // вказуєм назву файла звідки імпортуємо, назву зсінної можна поміняти (ексаортували users а полуличи obj)
 // або можемо зробити при імпорті дистроктуризацію, тобто взяти щось одне:
--- Node.js/methods.js
+++ Node.js/methods.js
@@ -1,8 +1,8 @@
-//------------------------JSON.parse(data) // - Преобразует строку в формате JSON → в JavaScript-объект.
+//------------------------JSON.parse(data) // - Преобразует строку в формате JSON → в JavaScript-объект або мвсив обьєктів.
 // const jsonString = '{"name": "Alex", "age": 25}';
 // const user = JSON.parse(jsonString);
-// console.log(typeof jsonString, jsonString); // string {"name": "Alex", "age": 25}
-// console.log(typeof user, user); // object { name: 'Alex', age: 25 } 
+// console.log(typeof jsonString, jsonString);  // string {"name": "Alex", "age": 25}
+// console.log(typeof user, user);              // object { name: 'Alex', age: 25 } 
 
 
 //--------------------------JSON.stringify(data) // Преобразует JavaScript-объект → в строку формата JSON.
@@ -10,3 +10,15 @@ const user = { name: "Alex", age: 25 };
 const jsonString = JSON.stringify(user);
 console.log(typeof user, user); // object { name: 'Alex', age: 25 }
 console.log(typeof jsonString, jsonString); //string {"name":"Alex","age":25}
+
+
+//------res.send() - збудує HTML структуру, як ми задаєм с початку:
+const html = books.map(book => `
+      <h1>${book.title}</h1>
+      <span>${book.description}</span>
+    `).join("");
+  res.send(html);  // в результаті буде звичайна веб-сторінка: White Iklo
+                                                            // random text about the book
+
+//------res.json(books) - просто передасть json, як він є
+
