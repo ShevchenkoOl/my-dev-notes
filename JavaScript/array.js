d142160e31550aecb72fc5c551292901da7bd9cc
ShevchenkoOl <uzlabini@gmail.com>
2025-08-13 00:39:17 +0200

    added new folder JS

---
+++ JavaScript/array.js
@@ -0,0 +1,25 @@
+const contacts = [
+  {
+    "id": "qdggE76Jtbfd9eWJHrssH",
+    "name": "Chaim Lewis",
+    "email": "dui.in@egetlacus.ca",
+    "phone": "(294) 840-6685"
+  },
+  {
+    "id": "drsAJ4SHPYqZeG-83QTVW",
+    "name": "Kennedy Lane",
+    "email": "mattis.Cras@nonenimMauris.net",
+    "phone": "(542) 451-7038"
+  }
+]
+
+// .push(...) - використовується для додавання одного або кількох елементів у кінець масиву. Він змінює сам масив і повертає нову довжину масиву.
+// console.log(contacts.push("newObject", {id: 15, name: "Alex"})); // 4 - це нова довжина
+// console.log(contacts);
+
+// .filter(...) — метод масиву, який створює новий масив, залишаючи лише ті елементи, для яких умова в дужках повертає true.
+// console.log(contacts.filter(contact => contact.id != "drsAJ4SHPYqZeG-83QTVW")); // поверне новий масив, в якому не буде обєкта контакта з id "drsAJ4SHPYqZeG-83QTVW"ю Підхоьить для видалення.
+
+
+// .find(...) - шукає перший елемент масиву, який задовольняє певну умову, і повертає цей елемент. Якщо нічого не знайде — поверне undefined. 
+// console.log(contacts.find(contact => contact.id === "drsAJ4SHPYqZeG-83QTVW"));
